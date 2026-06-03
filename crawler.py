8cbb6fd6ecf9572f41726096e0f46f37fe92708f
Vinay <iiith.vinay2025@gmail.com>
2026-06-03 10:24:50 +0530

    Update crawler.py - Extract and store crawled page content.

--- crawler.py
+++ crawler.py
@@ -8,36 +8,25 @@ response = requests.get("https://en.wikipedia.org/wiki/Search_Engine", headers =
 # Convert the text obtained from the website into a navigate-able tree!
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Extract raw-text, without the HTML tags from the title!
-# print(soup.title.text)
-
-# Extract all the paragraph tags!
-# paragraphs = soup.find_all("p")
-# print("Paragraph Count:", len(paragraphs))
-
-# Displaying first five paragraphs!
-# for index in range(min(5, len(paragraphs))):
-    # text = paragraphs[index].text.strip()
-    # print("\nParagraph", index + 1)
-    # print(text)
-
 # Extract links from the web-page!
 links = soup.find_all("a")
 print("Link Count:", len(links))
 
-# Print link(s) to other articles - first 8 links!
+# Print content for 2 page(s)!
 articleLinks = deque()
-alreadyVisited = set()
+alreadyVisited = set() # Prevent visiting duplicate page(s)!
+discovered = set() # Prevent adding duplicate page(s) to queue!
 visitedCount = 0
 baseLink = "https://en.wikipedia.org"
 visitedPages = []
 
 for link in links:
     current = link.get("href")
-    if current and current.startswith("/wiki/") and ":" not in current:
+    if current and current.startswith("/wiki/") and ":" not in current and current not in discovered:
+        discovered.add(current)
         articleLinks.append(current)
 
-while visitedCount < 8 and len(articleLinks) != 0:
+while visitedCount < 2 and len(articleLinks) != 0:
     current = articleLinks.popleft()
     if current not in alreadyVisited:
         alreadyVisited.add(current)
@@ -45,17 +34,22 @@ while visitedCount < 8 and len(articleLinks) != 0:
         innerResponse = requests.get(fullLink, headers = {"User-Agent": "Mozilla/5.0"})
         innerSoup = BeautifulSoup(innerResponse.text, "html.parser")
 
-        # print(innerSoup.title.text)
-        # print(fullLink)
-        currentPage = {"Title":innerSoup.title.text, "URL":fullLink}
+        innerParagraphs = innerSoup.find_all("p")
+        content = ""
+        for passage in innerParagraphs:
+            content = content + passage.text + "\n"
+
+        currentPage = {"Title":innerSoup.title.text, "URL":fullLink, "Content":content}
         visitedPages.append(currentPage)
 
         innerLinks = innerSoup.find_all("a")
         for innerLink in innerLinks:
             innerCurrent = innerLink.get("href")
-            if innerCurrent and innerCurrent.startswith("/wiki/") and ":" not in innerCurrent and innerCurrent not in alreadyVisited:
+            if innerCurrent and innerCurrent.startswith("/wiki/") and ":" not in innerCurrent and innerCurrent not in alreadyVisited and innerCurrent not in discovered:
+                discovered.add(innerCurrent)
                 articleLinks.append(innerCurrent)
         visitedCount = visitedCount + 1
 
 for page in visitedPages:
-    print(page)
+    print(f"{page['Title']} - {page['URL']}")
+    print(page['Content'])
